b7e6c0804bb8c48ce90701b0e3faa5a975bcfb58
unknown <afif.azizy@gmail.com>
2021-11-06 15:28:40 +0700

    global nav

---
+++ app/Views/global_nav.php
@@ -0,0 +1,64 @@
+<div id="header" class="layout-horizontal">
+    <header class="mb-5">
+        <div class="header-top-right">
+            <!-- Burger button responsive -->
+            <a href="#" class="burger-btn d-block d-xl-none">
+                <i class="bi bi-justify fs-3"></i>
+            </a>
+        </div>
+        <nav class="main-navbar">
+            <div class="container">
+                <ul class="d-flex justify-content-around">
+                    <li class="menu-item">
+                        <div class="container">
+                            <div class="logo">
+                                <a href="index.html"><img src="/assets/images/logo/logo.png" width="100" height="30" class="d-inline-block align-top" alt="Logo" srcset=""></a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="menu-item ">
+                        <a href="#" class='menu-link'>
+                            <span>Login</span>
+                        </a>
+                    </li>
+
+                    <li class="menu-item">
+                        <a href="#" class='menu-link'>
+                            <span>Be a Mentor</span>
+                        </a>
+                    </li>
+
+                    <li class="menu-item">
+                        <a href="#" class='menu-link'>
+                            <span>My Learnings</span>
+                        </a>
+                    </li>
+
+                    <li class="menu-item w-25">
+                        <form  method="GET">
+                            <div class="form-group position-relative has-icon-left">
+                                <input type="text" class="form-control" placeholder="Search ">
+                                <div class="form-control-icon">
+                                    <i class="bi bi-search"></i>
+                                </div>
+                            </div>
+                        </form>
+                    </li>
+
+                    <li class="menu-item">
+                        <a href="#" class='btn btn-success'>
+                            <span>Sign Up</span>
+                        </a>
+                    </li>
+
+                    <li class="menu-item">
+                        <a href="#" class='menu-link'>
+                            <span>Login</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</div>
