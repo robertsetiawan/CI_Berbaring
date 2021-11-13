e831fe9dc9601f9216e669095deacbaeff04b43c
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-13 09:58:59 +0700

    setup controllers based on models

---
+++ app/Controllers/Feedback.php
@@ -0,0 +1,5 @@
+<?php
+
+use App\Controllers\BaseController;
+
+class Feedback extends BaseController{}
---
+++ app/Controllers/StudentActivity.php
@@ -0,0 +1,5 @@
+<?php
+
+use App\Controllers\BaseController;
+
+class StudentActivity extends BaseController{}
