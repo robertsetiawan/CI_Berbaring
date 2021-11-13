de1b05f6677623aa014e1471c63ab7c55048bc4f
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-13 15:03:57 +0700

    fix namespace in models

--- app/Controllers/Course.php
+++ app/Controllers/Course.php
@@ -1,5 +1,18 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\CategoryModel;
+use App\Models\CourseModel;
 
-class Course extends BaseController{}
+class Course extends BaseController{
+    public function __construct()
+    {
+        $this->categories = new CategoryModel();
+        $this->courses = new CourseModel();
+    }
+
+    public function index()
+    {
+        return view('add_course');
+    }
+}
--- app/Models/CategoryModel.php
+++ app/Models/CategoryModel.php
@@ -1,5 +1,10 @@
 <?php
+namespace App\Models;
 
 use CodeIgniter\Model;
 
-class CategoryModel extends Model{}
+class CategoryModel extends Model{
+    protected $table = 'category';
+    protected $primaryKey = 'category_id';
+    protected $allowedFields = ['name'];
+}
