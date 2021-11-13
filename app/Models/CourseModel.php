0f982a346a23e2092de11e410b854b4a10226f91
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-13 20:36:13 +0700

    setup add course

--- app/Models/CourseModel.php
+++ app/Models/CourseModel.php
@@ -3,4 +3,28 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class CourseModel extends Model{}
+class CourseModel extends Model{
+    protected $table = 'course';
+    protected $primaryKey = 'c_id';
+    protected $allowedFields = ['c_name', 'c_imagepath', 'c_desc', 'c_price', 'category_id'];
+
+    public $validationRules = [
+        'c_name' => 'required',
+        'c_desc' => 'required|max_length[1000]',
+        'category_id' => 'required|greater_than[0]',
+    ];
+
+    public $errorMessage = [
+        'c_name' => [
+            'required' => 'Nama course harus diisi'
+        ],
+        'c_desc' => [
+            'required' => 'Deskripsi course harus diisi',
+            'max_length' => 'Panjang karakter maksimal adalah 1000'
+        ],
+        'category_id' => [ 
+            'required' => 'Kategori harus dipilih',
+            'greater_than' =>'Kategori harus dipilih'
+        ]
+    ];
+}
--- app/Views/add_course.php
+++ app/Views/add_course.php
@@ -8,31 +8,52 @@
 </head>
 
 <body>
-    <form method="POST" action="#" enctype="multipart/form-data">
+    <form method="POST" action="<?= base_url('/course/add') ?>" enctype="multipart/form-data">
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="text" name="c_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <label for="name" class="form-label">Course Name</label>
+            <input type="text" name="c_name" class="form-control" id="name" aria-describedby="emailHelp" value="<?= old('c_name') ?>">
         </div>
         <div class="mb-3">
-            <select class="form-select" name="category_id" aria-label="Default select example"></select>
+            <label for="category">Category</label>
+            <select class="form-select" name="category_id" id="category">
+                <option value="0">Pilih Kategori</option>
+                <?php if (!empty($categories)) : ?>
+                    <?php foreach ($categories as $category) : ?>
+                        <?php if (old('category_id') == $category['category_id']) : ?>
+                            <?php echo '<option value="' . $category['category_id'] . '" selected>' . $category['name'] . '</option>' ?>
+                        <?php else : ?>
+                            <option value="<?= $category['category_id'] ?>"><?= $category['name'] ?></option>
+                        <?php endif ?>
+                    <?php endforeach ?>
+                <?php endif ?>
+            </select>
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="text" name="c_desc" class="form-control" id="exampleInputPassword1">
+            <label for="desc" class="form-label">Description</label>
+            <textarea class="form-control" name="c_desc" id="desc" cols="30" rows="10"><?= old('c_desc') ?></textarea>
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-            <label class="form-check-label" for="inlineRadio1">1</label>
+            <input class="form-check-input" type="radio" name="paid_check" id="paid_check_yes" value="1" <?php if (old('paid_check') == 1) : ?> <?= 'checked' ?> <?php endif ?>>
+            <label class="form-check-label" for="paid_check_yes">Yes</label>
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-            <label class="form-check-label" for="inlineRadio2">2</label>
+            <input class="form-check-input" type="radio" name="paid_check" id="paid_check_no" value="0" <?php if (old('paid_check') == 0) : ?> <?= 'checked' ?> <?php endif ?>>
+            <label class="form-check-label" for="paid_check_no">No</label>
         </div>
         <div class="mb-3">
-            <label for="price" class="form-label">Password</label>
-            <input type="number" name="c_price" class="form-control" id="price">
+            <label for="price" class="form-label">Price</label>
+            <input type="number" value="<?= old('c_price') ?>" name="c_price" class="form-control" id="price">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="col-12 col-md-6">
+            <div class="form-group">
+                <label for="course_picture">Gambar Buku</label>
+                <p>Jenis File Image: png, jpg, atau jpeg. Maksimal ukuran file 2 mb
+                </p>
+                <input type="file" id="course_picture" name="course_picture" class="form-control" accept="image/*">
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Save as draft</button>
+        <button type="button">Publish</button>
     </form>
 </body>
 
