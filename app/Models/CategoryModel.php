ff7e4dda3ba4d48c2470d82fdddc976d04e134a5
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-13 15:05:41 +0700

    Update CategoryModel.php

--- app/Models/CategoryModel.php
+++ app/Models/CategoryModel.php
@@ -3,8 +3,4 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class CategoryModel extends Model{
-    protected $table = 'category';
-    protected $primaryKey = 'category_id';
-    protected $allowedFields = ['name'];
-}
+class CategoryModel extends Model{}
