cd6eb46c58c7ea3bdc0f895bd74f6a3b3512418f
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-18 00:16:31 +0700

    added new method saveCourse and info

--- app/Models/CourseModel.php
+++ app/Models/CourseModel.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace App\Models;
 
 use CodeIgniter\Model;
 
-class CourseModel extends Model{
+class CourseModel extends Model
+{
     protected $table = 'course';
     protected $primaryKey = 'c_id';
     protected $allowedFields = ['c_name', 'c_imagepath', 'c_desc', 'c_price', 'category_id'];
@@ -22,9 +24,32 @@ class CourseModel extends Model{
             'required' => 'Deskripsi course harus diisi',
             'max_length' => 'Panjang karakter maksimal adalah 1000'
         ],
-        'category_id' => [ 
+        'category_id' => [
             'required' => 'Kategori harus dipilih',
-            'greater_than' =>'Kategori harus dipilih'
+            'greater_than' => 'Kategori harus dipilih'
         ]
     ];
+
+    public function saveCourse($c_id, $c_name, $c_imagepath, $c_desc, $c_price, $category_id, $user_id)
+    {
+
+        $this->db->transBegin();
+
+        $this->db->query('INSERT INTO course (c_id, c_name, c_imagepath, c_desc, c_price, category_id) VALUES (' . $c_id . ',' . $c_name . ',' . $c_imagepath . ',' . $c_desc . ',' . $c_price . ',' . $category_id . ') ');
+
+        $this->db->query('INSERT INTO mentor_activity (created_date, published_date, user_id, c_id) VALUES (NOW(), NULL,' . $user_id . ',' . $c_id . ')');
+
+        if ($this->db->transStatus() === false) {
+            $this->db->transRollback();
+        } else {
+            $this->db->transCommit();
+        }
+    }
+
+    public function info($c_id)
+    {
+        $query = $this->db->query('SELECT * FROM course c INNER JOIN category t ON c.category_id = t.category_id WHERE c.c_id=' .$c_id. ';');
+
+        return $query->getRowArray();
+    }
 }
