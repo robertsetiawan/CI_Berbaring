fe17a81ee6dbddb69e9c5beb0cf9b8cc92093192
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-29 13:16:52 +0700

    added edit course validation

--- app/Models/MentorActivityModel.php
+++ app/Models/MentorActivityModel.php
@@ -3,4 +3,14 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class MentorActivityModel extends Model{}
+class MentorActivityModel extends Model{
+    protected $table = 'mentor_activity';
+    protected $primaryKey = 'activity_mentor_id';
+    protected $allowedFields = ['activity_mentor_id','created_date', 'published_date', 'user_id', 'c_id'];
+
+
+    public function getCouserCreatorId($c_id)
+    {
+        return $this->db->query('SELECT user_id FROM mentor_activity WHERE c_id=' .$c_id)->getRow();
+    }
+}
