3d64f1ee32e7703cd848a614093dc2214ffeb42e
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2021-12-01 14:47:13 +0700

    menambahkan view course page

--- app/Controllers/StudentActivity.php
+++ app/Controllers/StudentActivity.php
@@ -1,20 +1,29 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\StudentActivityModel;
 
-class StudentActivity extends BaseController{
+class StudentActivity extends BaseController
+{
 
-    function __construct(){
+    function __construct()
+    {
         $this->activity = new StudentActivityModel;
     }
 
-    function page($curr){
+    function page($curr)
+    {
         $data = [
             'courses' => $this->activity->getUserStudentActivity(session()->get('id'), $curr),
             'totalCourses' => $this->activity->getUserStudentActivityCount(session()->get('id')),
             'page' => $curr
         ];
         return view('homepage_pelajar.php', $data);
+    }
+
+    function coursePage()
+    {
+        return view('course_page.php');
     }
 }
---
+++ app/Views/course_page.php
@@ -0,0 +1,181 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course Page</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/bootstrap.css">
+
+    <link rel="stylesheet" href="/assets/vendors/iconly/bold.css">
+    <link rel="stylesheet" href="/assets/vendors/jquery-datatables/jquery.dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="/assets/vendors/fontawesome/all.min.css">
+
+    <link rel="stylesheet" href="/assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="/assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="/assets/css/app.css">
+    <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/x-icon">
+</head>
+
+<body>
+    <div id="app">
+        <!--pastiin include global nav di semua page! -->
+        <?php include('global_nav.php'); ?>
+        <div id="main" class="layout-horizontal">
+
+            <div class="content-wrapper container">
+
+                <div class="page-content">
+                    <!-- Basic Tables start -->
+                    <section class="section">
+                        <div class="container">
+                            <div class="row align-items-start">
+                                <div class="col">
+                                    <div class="row align-items-start">
+                                        <!--Judul-->
+                                        <h5 class="my-4" id="course_title">Developing Android App with Kotlin</h5>
+                                        <div class="col">
+                                            <!--Card course-->
+                                            <div class="card" style="width: 18rem;">
+                                                <img src="https://miro.medium.com/max/2625/1*0S2V9Vt6xRBpPkV4Y3zj9g.jpeg" class="card-img-top" alt="...">
+                                                <div class="card-body">
+                                                    <h6 class="card-title">Developing Android App with Kotlin</h6>
+                                                    <p class="card-text">Tatang Sutama</p>
+                                                    <a href="#" class="card-link">Rp1.000.000</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <!--yang didapat-->
+                                            <div class="card" style="width: 19rem;">
+                                                <div class="card-body">
+                                                    <table class="table table-borderless">
+                                                        <tbody>
+                                                            <tr>
+                                                                <th><i class='fas fa-file-video' style='font-size:14px'></th>
+                                                                <td>39 Videos</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <th><i class='fas fa-comment-alt' style='font-size:14px'></th>
+                                                                <td>Discuss with the mentor</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <th><i class='fas fa-folder-open' style='font-size:14px'></th>
+                                                                <td>5 Downloadable resource</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <th><i class='fas fa-clock' style='font-size:14px'></th>
+                                                                <td>Lifetime access</td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                            <div class="enroll">
+                                                <!--Enroll-->
+                                                <button class="btn btn-primary">Enroll now</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-5">
+                                        <h5 class="my-4" id="course_title">Know Your Mentor</h5>
+                                        <!--Mentor-->
+                                        <div class="row align-items-start">
+                                            <div class="col">
+                                                <img class="mb-1" src="https://awsimages.detik.net.id/community/media/visual/2020/03/02/2a63b94a-6c0c-4a98-bb8a-22d39ba1c449_43.jpeg?w=700&q=90" height="120" width="120" style="object-fit: cover; border-radius: 50%">
+                                            </div>
+                                            <div class="col">
+                                                <p><b>Tatang Sutama</b></p>
+                                                <p>Lecturer</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <h5 class="my-4" id="course_title">Chapter</h5>
+                                    <!--Card Chapter-->
+                                    <div class="card" style="width: 19rem;">
+                                        <div class="card-body">
+                                            <table class="table table-borderless">
+                                                <tbody>
+                                                    <tr>
+                                                        <th><i class='fas fa-lock' style='font-size:14px'></th>
+                                                        <td>Chapter 1: Introduction</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th><i class='fas fa-lock' style='font-size:14px'></th>
+                                                        <td>Chapter 1: Introduction</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th><i class='fas fa-lock' style='font-size:14px'></th>
+                                                        <td>Chapter 1: Introduction</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th><i class='fas fa-lock' style='font-size:14px'></th>
+                                                        <td>Chapter 1: Introduction</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th><i class='fas fa-lock' style='font-size:14px'></th>
+                                                        <td>Chapter 1: Introduction</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th><i class='fas fa-lock' style='font-size:14px'></th>
+                                                        <td>Chapter 1: Introduction</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th><i class='fas fa-lock' style='font-size:14px'></th>
+                                                        <td>Chapter 1: Introduction</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th><i class='fas fa-lock' style='font-size:14px'></th>
+                                                        <td>Chapter 1: Introduction</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th><i class='fas fa-lock' style='font-size:14px'></th>
+                                                        <td>Chapter 1: Introduction</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th><i class='fas fa-lock' style='font-size:14px'></th>
+                                                        <td>Chapter 1: Introduction</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+            <footer>
+                <div class="container">
+                    <div class="footer clearfix mt-5 mb-0 text-muted">
+                        <div class="float-start">
+                            <p>2021 &copy; Mazer</p>
+                        </div>
+                        <div class="float-end">
+                            <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a href="http://ahmadsaugi.com">A. Saugi</a></p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+    </div>
+    <script src="/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="/assets/vendors/apexcharts/apexcharts.js"></script>
+    <script src="/assets/js/pages/dashboard.js"></script>
+
+    <script src="/assets/js/pages/horizontal-layout.js"></script>
+    <script src="/assets/vendors/jquery/jquery.min.js"></script>
+    <script src="/assets/vendors/jquery-datatables/jquery.dataTables.min.js"></script>
+    <script src="/assets/vendors/jquery-datatables/custom.jquery.dataTables.bootstrap5.min.js"></script>
+    <script src="/assets/vendors/fontawesome/all.min.js"></script>
+</body>
+
+</html>
