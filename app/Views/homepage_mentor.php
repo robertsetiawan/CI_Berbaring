2a814819c3460ad2ffccc25c47a440b366b6235d
Jeremy Edbert Widjaja <widjajajeremy61@gmail.com>
2021-12-05 15:39:46 +0700

    fix card homepage mentor

--- app/Views/homepage_mentor.php
+++ app/Views/homepage_mentor.php
@@ -1,33 +1,44 @@
 <?php
 //untuk keperluan card homepage mentor
-function displayMentorCard($title="judul", $content="content", $published, $anchor, $picture="/assets/images/samples/banana.jpg"){
-    if (strlen($content)>50){
-        $deskripsi = substr($content, 0, 50);
+function displayMentorCard($title = "judul", $content = "content", $published, $anchor, $picture = "/assets/images/samples/banana.jpg")
+{
+    if (strlen($title) > 22) {
+        $title = substr($title, 0, 22);
+        $title .= "...";
+    } else {
+        $title = $title;
+    }
+
+    if (strlen($content) > 27) {
+        $deskripsi = substr($content, 0, 27);
         $deskripsi .= "...";
-    }else{
+    } else {
         $deskripsi = $content;
     }
+    
     $publish = "Published";
-    if ($published == NULL){
+    $background = "#097969";
+    if ($published == NULL) {
         $publish = "Unpublished";
+        $background = "#C70039";
     }
     echo '
     <div class="col">
-    <a href='.base_url('/course'.'/'.$anchor.'/info').'>
-    <div class="card" style="max-width: 20rem; max-height: 25rem;">
+    <a href=' . base_url('/course' . '/' . $anchor . '/info') . '>
+    <div class="card" style="max-height: 100vw">
         <div class="card-content style: \'position: relative;\'">
-            <img class="img-fluid w-100" src="'.$picture.'" style: "object-fit= cover; height: 12vw" alt="image" style= "object-fit: cover; height: 12vw">
+            <img class="card-img-top img-fluid" src="' . $picture . '" alt="image">
             <span 
             style="position: absolute; 
             bottom: 0px; right: 0px; 
-            background: #3C64B1;
-            opacity: 0.5; color: white; 
-            padding-left: 16px; padding-right: 16px;">'.$publish.'</span>
+            background:' . $background . ';
+            opacity: 1; color: white; 
+            padding-left: 16px; padding-right: 16px;">' . $publish . '</span>
         </div>
         <div class="card-body">
-            <h4 class="card-title">'.$title.'</h4>
+            <h4 class="card-title">' . $title . '</h4>
             <p class="card-text">
-                '.$deskripsi.'
+                ' . $deskripsi . '
             </p>
         </div>
     </div>
@@ -44,11 +55,11 @@ function displayMentorCard($title="judul", $content="content", $published, $anch
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage Mentor</title>
-    
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/bootstrap.css">
-    
+
     <link rel="stylesheet" href="/assets/vendors/iconly/bold.css">
     <link rel="stylesheet" href="/assets/vendors/jquery-datatables/jquery.dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="/assets/vendors/fontawesome/all.min.css">
@@ -57,11 +68,15 @@ function displayMentorCard($title="judul", $content="content", $published, $anch
     <link rel="stylesheet" href="/assets/vendors/bootstrap-icons/bootstrap-icons.css">
     <link rel="stylesheet" href="/assets/css/app.css">
     <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/x-icon">
-    <style>
+    <link rel="stylesheet" href="/assets/css/card.css">
+    <!-- <style>
         footer {
-            position: relative; bottom: 0; width: 100%; margin-top: 200px;
+            position: relative;
+            bottom: 0;
+            width: 100%;
+            margin-top: 200px;
         }
-    </style>
+    </style> -->
 </head>
 
 <body>
@@ -69,38 +84,40 @@ function displayMentorCard($title="judul", $content="content", $published, $anch
         <!--pastiin include global nav di semua page! -->
         <?php include('global_nav.php'); ?>
         <div id="main" class="layout-horizontal">
-            <div class="content-wrapper container"> 
+            <div class="content-wrapper container">
                 <br>
                 <div class="page-heading">
                     <h4>Create New Course</h4>
-                </div>  
-                
+                </div>
+
                 <div class="page-content">
                     <section class="row row-cols-3 row-cols-md-4 g-4">
-                            <div class="col">
-                                <a href="<?= base_url('/course')?>">
-                                <div class="card" style="max-width: 20rem; max-height: 25rem;">
+                        <div class="col">
+                            <a href="<?= base_url('/course') ?>">
+                                <div class="card">
                                     <div class="card-content style: 'position: relative;'">
-                                        <img class="img-fluid w-100" src="<?= base_url('assets/images/addcourse.png') ?>" style= "object-fit: cover" alt="image">
+                                        <img class="card-img-top img-fluid" src="<?= base_url('assets/images/addcourse.png') ?>" alt="image">
                                     </div>
                                     <div class="card-body">
                                         <h4 class="card-title">New Course</h4>
+                                        <p class="card-text" style="opacity: 0;">.</p>
                                     </div>
                                 </div>
-                                </a>
-                            </div>
+                            </a>
+                        </div>
 
-                            <?php
-                            //created course
-                            foreach ($courses as $course){
-                                displayMentorCard(
-                                    $course['c_name'], 
-                                    $course['c_desc'], 
-                                    $course['published_date'], 
-                                    $course['c_id'], 
-                                    '/uploads'.'/'.$course['c_id'].'/'.$course['c_imagepath']);
-                            }
-                            ?>
+                        <?php
+                        //created course
+                        foreach ($courses as $course) {
+                            displayMentorCard(
+                                $course['c_name'],
+                                $course['c_desc'],
+                                $course['published_date'],
+                                $course['c_id'],
+                                '/uploads' . '/' . $course['c_id'] . '/' . $course['c_imagepath']
+                            );
+                        }
+                        ?>
                     </section>
                 </div>
             </div>
@@ -109,7 +126,7 @@ function displayMentorCard($title="judul", $content="content", $published, $anch
     </div>
     <script src="/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
     <script src="/assets/js/bootstrap.bundle.min.js"></script>
-    
+
     <script src="/assets/vendors/apexcharts/apexcharts.js"></script>
     <script src="/assets/js/pages/dashboard.js"></script>
 
